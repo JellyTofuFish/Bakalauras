bc34be24f02d99a39852dcdd07b1b9456814794f
edvinas <EAksamitauskas@gmail.com>
2019-02-11 17:51:02 +0200

    css, javascript configuration with encoder and webpack

---
+++ assets/js/layout.js
@@ -0,0 +1,7 @@
+import $ from'jquery';
+import Routing from './Routing';
+
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+import '../css/layout.scss';
