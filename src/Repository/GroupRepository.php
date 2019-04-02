ae3222c735da8a25cd332f6fb466f38d75d3500b
edvinas <EAksamitauskas@gmail.com>
2019-04-02 14:19:15 +0300

    prod env

--- src/Repository/GroupRepository.php
+++ src/Repository/GroupRepository.php
@@ -2,7 +2,7 @@
 
 namespace App\Repository;
 
-use App\Entity\groupList;
+use App\Entity\GroupList;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
